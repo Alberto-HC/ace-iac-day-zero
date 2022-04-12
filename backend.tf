7aedd299802d97f54be378df63f1a8ac1c25361f
Alberto-HC <51166954+Alberto-HC@users.noreply.github.com>
2022-04-12 22:30:57 +0200

    Update backend.tf
    Modify organization.

--- backend.tf
+++ backend.tf
@@ -3,6 +3,7 @@ terraform {
   backend "remote" {
     hostname = "app.terraform.io"
     # organization = "<replace-with-your-Terraform-Cloud-organization-and-uncomment>"
+    organization = "Aviatrix-IAC-Lab"
     workspaces {
       name = "ace-iac-day-zero"
     }
